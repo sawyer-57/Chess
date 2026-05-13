d04ad77610b09e19b65b16ecdbd57ade6fc4147b
Sawyer Whipperman <sawyer57@byu.edu>
2026-05-12 22:13:46 -0600

    p1 isInCheckmate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -147,7 +147,24 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        //are they in check?
+        if (!isInCheck(teamColor)) {
+            return false; 
+        }
+        for (int row = 1; row <=8; row++) {
+            for (int col = 1; col <=8; col++) {
+                ChessPosition position = new ChessPosition(row, col); 
+                ChessPiece piece = board.getPiece(position); 
+                //is it their piece? 
+                if (piece != null && piece.getTeamColor() == teamColor) {
+                    Collection<ChessMove> moves = validMoves(position); 
+                    //is there a valid move to escape? 
+                    if (!moves.isEmpty()) {
+                        return false;
+                }
+            }
+        }
+        return true; 
     }
 
     /**
