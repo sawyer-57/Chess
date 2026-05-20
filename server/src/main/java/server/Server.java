ea3491c7398ccbf7982b0e5c0f5fded777ee287e
Sawyer Whipperman <sawyer57@byu.edu>
2026-05-20 11:44:42 -0600

    p3 register endpoints and exception handlers in server.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,8 @@
 package server;
 
 import io.javalin.*;
+import service.*;
+import dataaccess.*;
 
 public class Server {
 
@@ -10,7 +12,30 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        UserDAO userDAO = new MemoryUserDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
 
+        UserService userService = new UserService(userDAO, authDAO);
+        GameService gameService = new GameService(gameDAO, authDAO);
+        SessionService sessionService = new SessionService(userDAO, authDAO);
+        DatabaseService databaseService = new DatabaseService(userDAO, gameDAO, authDAO);
+
+        
+        ChessHandler handler = new ChessHandler(
+            userService,
+            gameService,
+            sessionService,
+            databaseService
+        );
+
+        javalin.post("/user", handler::register);
+        javalin.post("/session", handler::login);
+        javalin.delete("/session", handler::logout);
+        javalin.get("/game", handler::listGames);
+        javalin.post("/game", handler::createGame);
+        javalin.put("/game", handler::joinGame);
+        javalin.delete("/db", handler::clear);
     }
 
     public int run(int desiredPort) {
