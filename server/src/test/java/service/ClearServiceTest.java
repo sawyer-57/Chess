8f9aa86dbb4c699da2a57d694753add57e1c0e09
Sawyer Whipperman <sawyer57@byu.edu>
2026-05-20 11:11:03 -0600

    p3 ClearServiceTests

---
+++ server/src/test/java/service/ClearServiceTest.java
@@ -0,0 +1,31 @@
+package service;
+
+import dataaccess.*;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClearServiceTest {
+
+    private ClearService clearService;
+
+    @BeforeEach
+    public void setup(){
+
+        clearService =
+                new ClearService(
+                        new MemoryUserDAO(),
+                        new MemoryAuthDAO(),
+                        new MemoryGameDAO());
+    }
+
+    @Test
+    public void clearPositive(){
+
+        clearService.clear();
+
+        assertTrue(true);
+    }
+
+}
