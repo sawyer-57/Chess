e3e77902c69c6fc5308dd27b595f864344de17e7
Sawyer Whipperman <sawyer57@byu.edu>
2026-05-20 22:25:35 -0600

    p3 fix

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -18,10 +18,14 @@ public class ClearService {
         this.gameDAO=gameDAO;
     }
 
-    public void clear() throws DataAccessException{
-
-        userDAO.clear();
-        authDAO.clear();
-        gameDAO.clear();
+    public void clear() {
+        try {
+            userDAO.clear();
+            authDAO.clear();
+            gameDAO.clear();
+        }
+        catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
