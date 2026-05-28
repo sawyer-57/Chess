8871f868a280816bf26e543e27c34b192cb6ec12
Sawyer Whipperman <sawyer57@byu.edu>
2026-05-27 21:01:46 -0600

    p4 implement userDAO

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -5,14 +5,67 @@ import model.UserData;
 public class MySqlUserDAO implements UserDAO {
     @Override
     public void createUser(UserData user) throws DataAccessException {
+        String sql = """
+        INSERT INTO user
+        (username, password, email)
+        VALUES (?, ?, ?)
+        """;
+
+        try (var conn = DatabaseManager.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, user.username());
+            stmt.setString(2, user.password());
+            stmt.setString(3, user.email());
+
+            stmt.executeUpdate();
+
+        } catch (Exception e) {
+            throw new DataAccessException("Error creating user", e);
+        }
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return null;
+        String sql = """
+        SELECT username, password, email
+        FROM user
+        WHERE username = ?
+        """;
+
+        try (var conn = DatabaseManager.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, username);
+
+            var rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new UserData(
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("email")
+                );
+            }
+
+            return null;
+
+        } catch (Exception e) {
+            throw new DataAccessException("Error getting user", e);
+        }
     }
 
     @Override
     public void clear() throws DataAccessException {
+        String sql = "DELETE FROM user";
+
+        try (var conn = DatabaseManager.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+
+            stmt.executeUpdate();
+
+        } catch (Exception e) {
+            throw new DataAccessException("Error clearing users", e);
+        }
     }
 }
