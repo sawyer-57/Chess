6f1003dec331d1c557c01397272718fbe18dd02c
Sawyer Whipperman <sawyer57@byu.edu>
2026-06-10 21:53:17 -0600

    p6 connect command

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -4,7 +4,21 @@ import io.javalin.websocket.WsConnectContext;
 import io.javalin.websocket.WsCloseContext;
 import io.javalin.websocket.WsMessageContext;
 
+import com.google.gson.Gson;
+import io.javalin.websocket.*;
+
+import websocket.commands.UserGameCommand;
+
+import model.GameData;
+import dataaccess.GameDAO;
+import dataaccess.MySqlGameDAO;
+
+import websocket.messages.LoadGameMessage;
+
 public class WebSocketHandler {
+
+    private final Gson gson = new Gson();
+    private final GameDAO gameDAO = new MySqlGameDAO();
 
     public void onConnect(WsConnectContext ctx) {
         System.out.println("WebSocket connected");
@@ -15,6 +29,36 @@ public class WebSocketHandler {
     }
 
     public void onMessage(WsMessageContext ctx) {
-        System.out.println("Message received: " + ctx.message());
+        String message = ctx.message();
+
+        UserGameCommand command =
+                gson.fromJson(message, UserGameCommand.class);
+
+        if (command.getCommandType() == UserGameCommand.CommandType.CONNECT) {
+            connect(command, ctx);
+        }
+
+        System.out.println("Received command: "
+                + command.getCommandType());
     }
+
+
+    private void connect(UserGameCommand command,
+                         WsMessageContext ctx) {
+        try {
+
+            GameData gameData =
+                    gameDAO.getGame(command.getGameID());
+
+            LoadGameMessage message =
+                    new LoadGameMessage(gameData.game());
+
+            ctx.send(gson.toJson(message));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
 }
---
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -0,0 +1,17 @@
+package websocket.messages;
+
+import chess.ChessGame;
+
+public class LoadGameMessage extends ServerMessage {
+
+    private final ChessGame game;
+
+    public LoadGameMessage(ChessGame game) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+}
